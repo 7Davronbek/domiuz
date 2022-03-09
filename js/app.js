d1e73eb83b8e9f0e4a3e6290c1c9e52061b830d1
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-09 14:24:33 +0500

    domiuz

--- js/app.js
+++ js/app.js
@@ -45,25 +45,25 @@
 //     document.querySelector('.mobile').classList.toggle('active')
 // })
 
-$( ".button_su_inner" ).mouseenter(function(e) {
+$( ".myBtn_inner" ).mouseenter(function(e) {
   var parentOffset = $(this).offset(); 
  
   var relX = e.pageX - parentOffset.left;
   var relY = e.pageY - parentOffset.top;
-  $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-  $(this).prev(".su_button_circle").removeClass("desplode-circle");
-  $(this).prev(".su_button_circle").addClass("explode-circle");
+  $(this).prev(".myBtn_circle").css({"left": relX, "top": relY });
+  $(this).prev(".myBtn_circle").removeClass("desplode-circle");
+  $(this).prev(".myBtn_circle").addClass("explode-circle");
 
 });
 
-$( ".button_su_inner" ).mouseleave(function(e) {
+$( ".myBtn_inner" ).mouseleave(function(e) {
 
     var parentOffset = $(this).offset(); 
 
     var relX = e.pageX - parentOffset.left;
     var relY = e.pageY - parentOffset.top;
-    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-    $(this).prev(".su_button_circle").removeClass("explode-circle");
-    $(this).prev(".su_button_circle").addClass("desplode-circle");
+    $(this).prev(".myBtn_circle").css({"left": relX, "top": relY });
+    $(this).prev(".myBtn_circle").removeClass("explode-circle");
+    $(this).prev(".myBtn_circle").addClass("desplode-circle");
 
 });
