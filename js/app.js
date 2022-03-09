471c65d64eaed7d6a603113e3515322dd91fb378
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-09 16:05:25 +0500

    domiuz

--- js/app.js
+++ js/app.js
@@ -67,3 +67,22 @@ $( ".myBtn_inner" ).mouseleave(function(e) {
     $(this).prev(".myBtn_circle").addClass("desplode-circle");
 
 });
+
+
+
+
+const buttons = document.querySelectorAll('.myCircle')
+buttons.forEach((button) => {
+  button.onclick = function(e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    let ripple = document.createElement('span')
+
+    ripple.style.left = `${x}px`
+    ripple.style.top = `${y}px`
+    this.appendChild(ripple)
+    setTimeout(function() {
+      ripple.remove()
+    }, 600)
+  }
+})
