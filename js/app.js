8571b5d7b15c749f5fd23c5d851b900b9513fc9b
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-09 13:39:37 +0500

    domiuz

--- js/app.js
+++ js/app.js
@@ -22,25 +22,48 @@
 //   }
 // }
 
-const spiner = document.querySelector(".spiner");
+// const spiner = document.querySelector(".spiner");
 
-document.addEventListener("load", () => {
-  spiner.style.display = "none ";
+// document.addEventListener("load", () => {
+//   spiner.style.display = "none ";
+// });
+
+// function navbar() {
+//   const nav = document.querySelector(".navBar");
+//   let scrolling = window.pageYOffset > 200;
+
+//   nav.classList.toggle("active", scrolling);
+// }
+
+// window.addEventListener("scroll", () => {
+//   navbar();
+// });
+
+
+// document.querySelector('.burger').addEventListener('click', () => {
+//     document.querySelector('.burger').classList.toggle('burgered')
+//     document.querySelector('.mobile').classList.toggle('active')
+// })
+
+$( ".button_su_inner" ).mouseenter(function(e) {
+  var parentOffset = $(this).offset(); 
+ 
+  var relX = e.pageX - parentOffset.left;
+  var relY = e.pageY - parentOffset.top;
+  $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+  $(this).prev(".su_button_circle").removeClass("desplode-circle");
+  $(this).prev(".su_button_circle").addClass("explode-circle");
+
 });
 
-function navbar() {
-  const nav = document.querySelector(".navBar");
-  let scrolling = window.pageYOffset > 200;
+$( ".button_su_inner" ).mouseleave(function(e) {
 
-  nav.classList.toggle("active", scrolling);
-}
+    var parentOffset = $(this).offset(); 
 
-window.addEventListener("scroll", () => {
-  navbar();
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+    $(this).prev(".su_button_circle").removeClass("explode-circle");
+    $(this).prev(".su_button_circle").addClass("desplode-circle");
+
 });
-
-
-document.querySelector('.burger').addEventListener('click', () => {
-    document.querySelector('.burger').classList.toggle('burgered')
-    document.querySelector('.mobile').classList.toggle('active')
-})
